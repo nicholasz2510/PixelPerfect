b4716def0c98eceb49ae12760d308298e23b3267
dwelicki <dwelicki@uw.edu>
2024-10-13 03:53:53 -0700

    small change to user schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,23 +2,11 @@ const mongoose = require('mongoose');
 const GLOBAL_ROOM = "670b583bacdb17a8c5c211af";
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-
-  password: {
-    type: String,
-    required: true,
-  },
-
   rooms: {
     type: [String],
     required: true,
     default: () => [GLOBAL_ROOM]
   }
-
-  
 });
 
 module.exports = mongoose.model('User', userSchema);
